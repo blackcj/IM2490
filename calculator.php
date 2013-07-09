82d5d9cbb24bfa0d18b5c6e7cec803dd2a5eb840
stu <stu@WI13-Image-187.local>
2013-07-08 21:51:04 -0500

    Added some PHP code for IM3420.

---
+++ calculator.php
@@ -0,0 +1,8 @@
+$values = "7 8 9 4 5 6 1 2 3 0 . =";
+$keys = explode(" ", $values);
+for($counter = 0; $counter < sizeof($keys); $counter ++)
+{
+    echo "<button class='num'>" . $keys[$counter] . "</button>";
+	if(($counter + 1) % 3 == 0)
+		echo "</br>";
+}
