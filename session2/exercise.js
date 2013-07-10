471ab0ed9434c4a891c03f079f1b0731aa2c5ce7
stu <stu@WI13-Image-187.local>
2013-07-10 18:55:28 -0500

    updated with session 2.

---
+++ session2/exercise.js
@@ -0,0 +1,8 @@
+// writecodeonline.com/javascript Use Firefox!
+document.write('<button onclick="myFunction()">Hello World</button>');
+
+function myFunction()
+{
+   var today = new Date();
+   alert(today);
+}
